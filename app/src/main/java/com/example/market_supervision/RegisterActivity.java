0a6a7380a9dacdc9d8fd187f2f6f44d5e9b44e07
pengtai <pengtai@flux.com.cn>
2021-03-08 17:53:47 +0800

    login an register

---
+++ app/src/main/java/com/example/market_supervision/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.market_supervision;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_register_step_one);
+    }
+}
